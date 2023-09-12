f1796f8a27d348e69ff9d2d3e4115e80fbf500fb
Mateus Kern <mateus.g.kern@gmail.com>
2023-09-11 21:18:56 -0300

    feat: Ajustando módulos para funcionar front-end

--- CalculoCDB/ClientApp/src/app/app.module.ts
+++ CalculoCDB/ClientApp/src/app/app.module.ts
@@ -1,34 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router'; import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { CalcularCdbComponent } from './calcular-cdb/calcular-cdb.component';
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    CalcularCdbComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    ReactiveFormsModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'calcular-cdb', component: CalcularCdbComponent }
     ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' }
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
